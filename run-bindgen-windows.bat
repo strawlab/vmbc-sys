7ad17597e2b72f367b0f36493bf7a1caf7d48509
Andrew Straw <strawman@astraw.com>
2022-07-27 13:52:15 -0700

    add instructions to install bindgen.exe

--- run-bindgen-windows.bat
+++ run-bindgen-windows.bat
@@ -1,3 +1,4 @@
 @REM Download llvm from https://releases.llvm.org/download.html
+@REM install bindgen with "cargo install bindgen"
 set LIBCLANG_PATH=C:\Program Files\LLVM\bin
 bindgen.exe "C:\Program Files\Allied Vision\Vimba_5.0\VimbaC\Include\VimbaC.h" --default-enum-style moduleconsts --bitfield-enum VmbFeatureFlagsType --bitfield-enum VmbFrameFlagsType --with-derive-partialeq --distrust-clang-mangling --raw-line #![allow(dead_code,non_upper_case_globals,non_camel_case_types,non_snake_case)] -o src\lib.rs
